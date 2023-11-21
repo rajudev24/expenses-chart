2b75aa2ef0863d1ed39a9e1d9ac20c0fccd6ef82
Raju Deb Nath <rajudevit94@gmail.com>
2023-11-21 09:32:41 +0600

    Design Done

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,26 @@ import { chartData, colors } from "./utils";
 function App() {
   const [selectedPeriod, setSelectedPeriod] = useState("1M");
   return (
-    <div className="container">
-      <h2>Expenses</h2>
-      <Tabs
-        selectedPeriod={selectedPeriod}
-        setSelectedPeriod={setSelectedPeriod}
-      />
-      <div className="chart">
-        <DoughnutChart
-          data={chartData}
-          selectedPeriod={selectedPeriod}
-          size={250}
-          colors={colors}
-        />
+    <div className="expenses-section ">
+      <div>
+        <h1 className="title">Expenses Chart</h1>
+        <div className="container">
+          <h2>Expenses</h2>
+          <Tabs
+            selectedPeriod={selectedPeriod}
+            setSelectedPeriod={setSelectedPeriod}
+          />
+          <div className="chart">
+            <DoughnutChart
+              data={chartData}
+              selectedPeriod={selectedPeriod}
+              size={250}
+              colors={colors}
+            />
+          </div>
+          <ColorCategory />
+        </div>
       </div>
-      <ColorCategory />
     </div>
   );
 }
